d0c6079dc4af085a5b416f8d63b87f130f85ce0a
Mikko Koski <mikkokos@gmail.com>
2016-06-15 10:34:53 +0300

    Info section 1 desktop version

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -33,10 +33,10 @@ class LandingPageController < ActionController::Metal
   def preview
     preview_version = parse_int(params[:preview_version])
     begin
-      structure = LandingPageStore.load_structure(community_id(request), preview_version)
+      # structure = LandingPageStore.load_structure(community_id(request), preview_version)
 
       # Uncomment for dev purposes
-      # structure = JSON.parse(data_str)
+      structure = JSON.parse(data_str)
 
       # Tell robots to not index and to not follow any links
       headers["X-Robots-Tag"] = "none"
@@ -145,6 +145,17 @@ class LandingPageController < ActionController::Metal
       "search_path": {"type": "path", "id": "search_path"}
     },
     {
+      "id": "info",
+      "kind": "info",
+      "variation": "one_column",
+      "title": "Section title goes here",
+      "paragraph": "Paragraph. Curabitur blandit tempus porttitor. Nulla vitae elit libero, a pharetra augue. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Nulla vitae elit libero.",
+      "button_title": "Section link",
+      "button_path": {"path": "https://google.com"},
+      "button_color": {"type": "marketplace_data", "id": "primary_color"},
+      "background_image": {"type": "assets", "id": "myinfoimage"}
+    },
+    {
       "id": "footer",
       "kind": "footer",
       "theme": "dark",
@@ -173,6 +184,8 @@ class LandingPageController < ActionController::Metal
   ],
 
   "composition": [
+    { "section": {"type": "sections", "id": "info"},
+      "disabled": false},
     { "section": {"type": "sections", "id": "private_hero"},
       "disabled": false},
     { "section": {"type": "sections", "id": "footer"},
@@ -184,10 +197,8 @@ class LandingPageController < ActionController::Metal
   ],
 
   "assets": [
-    {
-      "id": "myheroimage",
-      "src": "hero.jpg"
-    }
+    { "id": "myheroimage", "src": "hero.jpg" },
+    { "id": "myinfoimage", "src": "info.jpg" }
   ]
 }
 JSON
